3eb29e8fe656f3faac0e00b873c6803b857efede
Kaniz92 <kanishkasilva22@gmail.com>
2018-03-02 04:15:18 +0530

    init

---
+++ OrgEmployee/OrgEmployee/Scripts/ViewModels/Employee/IndexVM.js
@@ -0,0 +1,47 @@
+﻿@Model model
+$(function () {
+    ko.applyBindings(IndexVM);
+    IndexVM.getEmployees();
+});
+
+var IndexVM = {
+    Employees: ko.observableArray([]),
+    getEmployees: function () {
+        var self = this;
+        //$.ajax({
+        //    type: "GET",
+        //    url: '/Employee/Index',
+        //    contentType: "application/json; charset=utf-8",
+        //    dataType: "json",
+        //    success: function (data) {
+        //        console.log(data);
+        //        self.Employees(data); //Put the response in ObservableArray
+        //    },
+        //    error: function (err) {
+        //        alert(err.status + " : " + err.statusText);
+        //    }
+        //});
+    },
+    CreateNew: function () {
+        window.location.href = '/Employee/Create';
+    },
+    Back: function(){
+        window.location.href = '/Home';
+    }
+};
+
+self.EditEmployee = function (employee) {
+    window.location.href = '/Employee/Edit/' + employee.EmployeeID;
+};
+self.DeleteEmployee = function (employee) {
+    window.location.href = '/Employee/Delete/' + employee.EmployeeID;
+};
+
+//Model
+function Employees(data) {
+    console.log(data);
+    this.EmployeeID = ko.observable(data.EmployeeID);
+    this.FirstName = ko.observable(data.FirstName);
+    this.LastName = ko.observable(data.LastName);
+    this.JoiningDate = ko.observable(data.JoiningDate);
+}
---
+++ OrgEmployee/OrgEmployee/Scripts/ViewModels/Home/IndexVM.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    ko.applyBindings(IndexVM);
+});
+
+var IndexVM = {
+    viewEmployeeList: function () {
+        window.location.href = '/Employee/Index';
+    },
+    viewEnrollmentList: function () {
+        window.location.href = '/Enrollment/Index';
+    }
+};
